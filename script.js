2963c987f2623a07e7bcc56bbcbc9a0d67c0f82c
TiBappy <tibappy95@gmail.com>
2026-03-08 19:05:49 +0600

    Finally Fully customized github issues tracker website

--- script.js
+++ script.js
@@ -32,15 +32,11 @@ async function loadCards() {
   showLoading();
 
   const res = await fetch(
-    "https://phi-lab-server.vercel.app/api/v1/lab/issues"
+    "https://phi-lab-server.vercel.app/api/v1/lab/issues",
   );
-
   const data = await res.json();
-
   hideLoading();
-
   allIssues = data.data;
-
   displayCards(allIssues);
   updateCounters(allIssues);
 }
@@ -48,64 +44,43 @@ async function loadCards() {
 // Display Cards
 function displayCards(cards) {
   cardsContainer.innerHTML = "";
-
   cards.forEach((element) => {
     const singleCards = document.createElement("div");
-
-    // ⭐ Border Logic
     const borderColor =
       element.status === "open"
         ? "border-l-4 border-green-500"
         : "border-l-4 border-purple-500";
-
     singleCards.className = "h-full";
-
     singleCards.innerHTML = `
-    
     <div class="card bg-white shadow-lg p-4 space-y-3 h-full cursor-pointer ${borderColor}">
-    
-      <div class="flex justify-between">
-      
-        <img src="./assets/${
-          element.status === "closed" ? "Closed-Status.png" : "Open-Status.png"
-        }">
-        
+    <div class="flex justify-between">
+      <img src="./assets/${
+        element.status === "closed" ? "Closed-Status.png" : "Open-Status.png"
+      }">
         <div class="badge badge-soft badge-secondary">
         ${element.priority}
         </div>
-        
-      </div>
-
-      <div class="space-y-3">
-      
-        <h2 class="text-lg font-semibold">
+        </div>
+<div class="space-y-3">
+      <h2 class="text-lg font-semibold">
         ${element.title}
         </h2>
-
-        <p class="line-clamp-2 text-sm">
+<p class="line-clamp-2 text-sm">
         ${element.description}
         </p>
-
-      </div>
-
-      <div class="flex gap-2">
-
-        <div class="badge badge-outline bg-pink-100 badge-secondary">
+</div>
+<div class="flex gap-2">
+<div class="badge badge-outline bg-pink-100 badge-secondary">
         BUG
         </div>
-
-        <div class="badge badge-outline badge-warning bg-yellow-100">
+<div class="badge badge-outline badge-warning bg-yellow-100">
         HELP WANTED
         </div>
-
-      </div>
-
-      <hr>
-
-      <p class="text-[14px]">${element.author}</p>
-      <p class="text-[14px]">${element.createdAt}</p>
-
-    </div>
+</div>
+<hr>
+<p class="text-[14px]">${element.author}</p>
+<p class="text-[14px]">${element.createdAt}</p>
+</div>
     
     `;
 
@@ -120,31 +95,23 @@ function displayCards(cards) {
 // ALL
 document.getElementById("primary-btn").addEventListener("click", function () {
   setActiveButton("primary-btn");
-
   displayCards(allIssues);
-
   updateCounters(allIssues);
 });
 
 // OPEN
 document.getElementById("opened-btn").addEventListener("click", function () {
   setActiveButton("opened-btn");
-
   const openedIssues = allIssues.filter((issue) => issue.status === "open");
-
   displayCards(openedIssues);
-
   updateCounters(openedIssues);
 });
 
 // CLOSED
 document.getElementById("closed-btn").addEventListener("click", function () {
   setActiveButton("closed-btn");
-
   const closedIssues = allIssues.filter((issue) => issue.status === "closed");
-
   displayCards(closedIssues);
-
   updateCounters(closedIssues);
 });
 
@@ -153,7 +120,6 @@ function setActiveButton(id) {
   document
     .querySelectorAll("#toggle-btn button")
     .forEach((btn) => btn.classList.remove("btn-primary"));
-
   document.getElementById(id).classList.add("btn-primary");
 }
 
@@ -165,13 +131,10 @@ function updateCounters(cards) {
 // Search Issue
 searchInput.addEventListener("input", function () {
   const searchText = searchInput.value.toLowerCase();
-
   const filteredIssues = allIssues.filter((issue) =>
-    issue.title.toLowerCase().includes(searchText)
+    issue.title.toLowerCase().includes(searchText),
   );
-
   displayCards(filteredIssues);
-
   updateCounters(filteredIssues);
 });
 
